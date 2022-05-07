baa513a82d58caf9d76bf8a30eb4dcba7984eda2
JohannaFrd <piistal@yahoo.fr>
2022-05-07 18:34:12 +0200

    initial commit on main with fake password

---
+++ HornetProject/src/main/java/fr/hornet/hornetproject/HornetProjectApplication.java
@@ -0,0 +1,13 @@
+package fr.hornet.hornetproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HornetProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HornetProjectApplication.class, args);
+	}
+
+}
---
+++ HornetProject/src/test/java/fr/hornet/hornetproject/HornetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.hornet.hornetproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HornetProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
