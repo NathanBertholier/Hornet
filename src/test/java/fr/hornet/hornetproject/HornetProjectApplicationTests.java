16b0be9f37607291eda7d5590874376ee5f021bc
Nathan Bertholier <nathan.bertholier@gmail.com>
2022-05-08 12:25:09 +0200

    refactor

--- src/test/java/fr/hornet/hornetproject/HornetProjectApplicationTests.java
+++ src/test/java/fr/hornet/hornetproject/HornetProjectApplicationTests.java
@@ -3,11 +3,14 @@ package fr.hornet.hornetproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class HornetProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
